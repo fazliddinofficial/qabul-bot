9c06dcb35a692125c5660275b5bba48fb952cdbc
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2026-02-18 10:24:42 +0500

    add: new feat

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import { POSITION_OPTIONS } from "./constants.js";
+
 config();
 
 const db_url = process.env.DB_URL;
 
-const creatingUserLimit = 10;
+const creatingUserLimit = Object.values(POSITION_OPTIONS).length;
 
 const userSchema = new mongoose.Schema({
   userId: {
@@ -12,9 +14,8 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  limit: {
-    type: Number,
-    default: 11,
+  usedFields: {
+    type: [String],
   },
 });
 
@@ -29,27 +30,35 @@ export const connectDB = async () => {
   }
 };
 
-export const createUser = async (userId) => {
-  await User.create({ userId, limit: creatingUserLimit });
-  return "User created";
-};
-
 export const checkUserExist = async (userId) => {
   const foundUser = await User.findOne({ userId });
   if (!foundUser) {
-    await createUser(userId);
-
-    return { message: "Sizda 2 ta urunish qoldi", status: true };
-  } else if (foundUser.limit > 0) {
-    foundUser.limit = foundUser.limit - 1;
-
-    await foundUser.save();
-
-    return {
-      message: "Siz yana bitta urunishdan foydalandingiz",
-      status: true,
-    };
-  } else if (foundUser.limit === 0) {
-    return { message: "Sizda urunish qolmagan", status: false };
+    await User.create({ userId });
   }
+};
+
+export const addTitleToUser = async (userId, field) => {
+  const foundUser = await User.findOne({ userId });
+
+  if (!foundUser) {
+    return "User not found";
+  }
+
+  foundUser.usedFields.push(field);
+  await foundUser.save();
+};
+
+export const checkUserPositions = async (userId, position) => {
+  const foundUser = await User.findOne({ userId });
+
+  if (!foundUser) {
+    return { status: false, message: `/start buyrug'ini bosing!` };
+  } else if (foundUser.usedFields.includes(position)) {
+    return {
+      status: false,
+      message: `Siz ${position}ga allaqachon resume jo'natib bo'lgansiz!`,
+    };
+  }
+  await addTitleToUser(userId, position);
+  return { status: true, message: "ok" };
 };
