5039fc2ea24313b47eaca999eab4dec60e7e5fae
fazliddin_quvatboyev <166515200+fazliddinofficial@users.noreply.github.com>
2026-02-21 17:19:47 +0500

    Feat/unique (#10)
    * fix: uniqueness logic
    
    * fix: issues
    
    * fix: issues

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Markup, Telegraf } from "telegraf";
 import { config as dotenv } from "dotenv";
 import { POSITION_KEYBOARD } from "./constants.js";
 import { questions } from "./questions.js";
+import { loadUsers, writeData, stringtype } from "./handleJson.js";
 
 dotenv();
 
@@ -9,6 +10,32 @@ const token = process.env.BOT_TOKEN;
 const CHANNEL_OR_GROUP_TOKEN = process.env.CHANNEL_OR_GROUP_TOKEN;
 
 const bot = new Telegraf(token);
+
+const users = loadUsers();
+
+function checkUserExist(userId, role) {
+  if (role == null || role == undefined) return { status: false };
+  userId = userId.toString();
+  for (const key in users) {
+    if (key === userId) {
+      const canUserApply = users[key].includes(role);
+      if (canUserApply) {
+        return {
+          status: false,
+          message: "Siz bu yo'nalish bo'yicha allaqachon ariza topshirgansiz.",
+        };
+      } else {
+        users[key].push(role);
+        const jsonValue = JSON.stringify(users);
+        writeData(jsonValue);
+        return {
+          status: true,
+          message: `Siz ${role} uchun ariza topshirdingiz.`,
+        };
+      }
+    }
+  }
+}
 
 const sessions = new Map();
 
@@ -144,7 +171,14 @@ bot.on("message", async (ctx) => {
       reply_markup: { remove_keyboard: true },
     });
   }
-  await sendToRecruiter(ctx, session);
+  const result = checkUserExist(userId, session.answers.position.toString());
+
+  if (result.status) {
+    await sendToRecruiter(ctx, session);
+  }else{
+    ctx.reply('Siz ishga bu yo`nalish bo`yicha allaqachon topshirib bo`lgansiz.');
+    sessions.delete(ctx.from.id);
+  }
 });
 
 async function sendToRecruiter(ctx, session) {
