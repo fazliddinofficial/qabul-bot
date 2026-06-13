85ed401e5d757cd241427d2f85dc196bebda24c9
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2026-06-13 10:23:58 +0500

    fix: issues

--- index.js
+++ index.js
@@ -86,6 +86,7 @@ bot.start(async (ctx) => {
       answers: {},
       questions: [...questions],
     });
+
     ctx.reply(
       `Assalomu alaykum! Botimizga xush kelibsiz. Botimizdan ishga birinchi marta topshirayapsizmi?`,
       Markup.inlineKeyboard([
@@ -93,20 +94,9 @@ bot.start(async (ctx) => {
         Markup.button.callback("❌ Yo'q, avval topshirganman", "secondTime"),
       ]),
     );
-    step: 0,
-    answers: {},
-    questions: [...questions],
-  });
-  ctx.reply(
-    `Assalomu alaykum! Botimizga xush kelibsiz. Botimizdan ishga birinchi marta topshirayapsizmi?`,
-    Markup.inlineKeyboard([
-      Markup.button.callback("✅ Ha, birinchi marta", "firstTime"),
-      Markup.button.callback("❌ Yo'q, avval topshirganman", "secondTime"),
-    ]),
-  );
   } catch (error) {
-    console.log(`❌ Start failed for ${userId}: ${err.message}`);
-    sessions.delete(userId);
+    console.log(`❌ Start failed for ${ctx.from.id}: ${error.message}`);
+    sessions.delete(ctx.from.id);
   }
 });
 
